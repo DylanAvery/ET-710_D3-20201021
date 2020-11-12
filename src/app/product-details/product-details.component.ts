d3b2c0c93b58db218a5136f2e85d3f8565dcfbc3
DylanAvery <dylavery712@gmail.com>
2020-11-12 14:28:22 -0500

    End of Class

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,9 +13,15 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
+addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+}
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
+
 
   ) { }
 
